6ee9405bb7c9c99c33417f3adbf1a813eba804e7
Kaja A <kaja_a@yahoo.com>
2020-03-25 11:34:13 +0000

    Update epi_covid.R

--- code/epi_covid.R
+++ code/epi_covid.R
@@ -3,12 +3,41 @@
 # To assess the health benefits of vaccination versus the risks of sustaining or 
 # suspending immunisation programmes in Africa during the COVID-19 pandemic. 
 
-# remove all objects from workspace
-rm (list = ls ())
-
 # load libraries
 library (data.table)
+library (ggplot2)
+library (tictoc)
+
+# remove all objects from workspace
+rm (list = ls ())
 
 # start time
 print (Sys.time ())
 tic ()
+
+# ------------------------------------------------------------------------------
+# functions
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Extract vaccine coverage estimates for 2018 from WUENIC 2018
+# ------------------------------------------------------------------------------
+wuenic_vaccine_coverage <- function (allburden) {
+
+} # end of function -- combine_burden_estimate
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# main program
+# ------------------------------------------------------------------------------
+
+# start of program
+print (Sys.time ())
+
+# extract vaccine coverage estimates for 2018 from WUENIC 2018
+wuenic <- wuenic_vaccine_coverage ()
+
+# end of program
+print (Sys.time ())
+toc ()
+# ------------------------------------------------------------------------------
