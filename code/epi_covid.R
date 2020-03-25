ccaab07a5b25d9b5c5a6921ba22199cdc5b3edc5
Kaja A <kaja_a@yahoo.com>
2020-03-25 11:22:10 +0000

    Create epi_covid.R

---
+++ code/epi_covid.R
@@ -0,0 +1,14 @@
+# epi_covid.R
+
+# To assess the health benefits of vaccination versus the risks of sustaining or 
+# suspending immunisation programmes in Africa during the COVID-19 pandemic. 
+
+# remove all objects from workspace
+rm (list = ls ())
+
+# load libraries
+library (data.table)
+
+# start time
+print (Sys.time ())
+tic ()
